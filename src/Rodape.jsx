59c8da4d312bc7b3ab09530df121bd21c1ae53b2
AlineBrPt <aline.webrj@gmail.com>
2023-11-16 16:38:28 +0000

    Correção de endereço de imagem

--- src/Rodape.jsx
+++ src/Rodape.jsx
@@ -1,5 +1,8 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
+import htmlLogo from './assets/html5_icon.svg';
+import cssLogo from './assets/css3_icon.svg';
+import javascriptLogo from './assets/javascript_icon.svg';
+import reactLogo from './assets/react.svg';
+import viteLogo from './assets/vite.svg';
 
 function Rodape() {
   return (
@@ -11,9 +14,9 @@ function Rodape() {
             <a href="www.youtube.com/@programacaoweb" target="_blank">Canal: Programação Web</a>
           </p>
           <div className='tecnologias'>
-          <img src="./src/assets/html5_icon.svg" className="logo" alt="Logo HTML5" />
-            <img src="./src/assets/css3_icon.svg" className="logo" alt="Logo CSS3" />
-            <img src="./src/assets/javascript_icon.svg" className="logo" alt="Logo JavaScript" />
+            <img src={htmlLogo} className="logo" alt="Logo HTML5" />
+            <img src={cssLogo} className="logo" alt="Logo CSS3" />
+            <img src={javascriptLogo} className="logo" alt="Logo JavaScript" />
             <a href="https://vitejs.dev" target="_blank">
               <img src={viteLogo} className="logo" alt="Vite logo" />
             </a>
